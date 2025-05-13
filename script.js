cebd1db2c94808301ec179a98440a162e3dc6a86
Amr Elkheir <AmrElkheir2005@outlook.com>
2025-05-13 16:46:55 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+document.getElementById("project1").onclick = function () {
+    window.location.href = "https://github.com/19xonsor/Amr-website.git";
+};
+
+document.getElementById("project2").onclick = function () {
+    window.location.href = "https://example.com/project2";
+};
+
+document.getElementById("project3").onclick = function () {
+    window.location.href = "https://example.com/project3";
+};
+
+document.getElementById("project4").onclick = function () {
+    window.location.href = "https://example.com/project4";
+};
